414078274b8e3af1239c77e8389c9bac21c0f1d0
Kimdavin123 <dvkim0119@naver.com>
2024-04-15 13:38:57 +0900

    add other page

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>kimdabin_testheav</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
